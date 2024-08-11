fa1e706c725ce5cbceb1c5fafe67de356e1dde94
OnFlApp <onflapp@gmail.com>
2024-08-11 15:33:28 +0200

    updates

--- Applications/Terminal.app/main.js
+++ Applications/Terminal.app/main.js
@@ -1,34 +1,30 @@
 const path = require('path');
+const mutil = require('shared/messages.js');
 
-function wsserver(cb) {
-  const WebSocketServer = require('websocket').server;
-  const http = require('http');
+function ws_handle_connection(connection) {
   const Pty = require("node-pty");
-
-  let server = http.createServer();
-  let wsServer = new WebSocketServer({
-    httpServer: server
+  let ptty = Pty.spawn("bash", [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.PWD,
+    env: process.env
   });
 
-  wsServer.on('request', function(request) {
-    let connection = request.accept(null, request.origin);
-    let ptty = Pty.spawn("bash", [], {
-      name: 'xterm-color',
-      cols: 80,
-      rows: 24,
-      cwd: process.env.PWD,
-      env: process.env
-    });
-
-    ptty.on('exit', function(code, signal) {
+  ptty.on('exit', 
+    function(code, signal) {
       console.log('process exit');
-    });
+    }
+  );
 
-    ptty.on('data', function(data) {
+  ptty.on('data', 
+    function(data) {
       connection.send(data);
-    });
+    }
+  );
 
-    connection.on('message', function(message) {
+  connection.on('message',
+    function(message) {
       let str = message.utf8Data;
       let i = str.indexOf('^[[');
       if (i != -1) {
@@ -40,32 +36,59 @@ function wsserver(cb) {
       if (str) {
         ptty.write(str);
       }
-    });
+    }
+  );
 
-    connection.on('close', function(reasonCode, description) {
+  connection.on('close',
+    function(reasonCode, description) {
       console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-  });
-
-  server.listen(0, function() {
-    cb(server);
-  });
+    }
+  );
 }
 
-const express = require('express');
-const app = express();
+function ws_server_ready(cb) {
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+function ws_server(cb) {
+  const WebSocketServer = require('websocket').server;
+  const http = require('http');
 
-app.use('/', express.static("."));
-
-let server = app.listen(0, function () {
-  let sport = server.address().port;
-  wsserver(function(ws) {
-    let wport = ws.address().port;
-    const utils = require('../../System/utils.js');
-    console.log(utils.make_message('DISPLAY', {'cmd':'show','url':`http://localhost:${sport}/index.html?data=${wport}`}));
+  let server = http.createServer();
+  let wsServer = new WebSocketServer({
+    httpServer: server
   });
-})
+
+  wsServer.on('request', function(request) {
+    let connection = request.accept(null, request.origin);
+    ws_handle_connection(connection);
+  });
+
+  server.listen(0, 
+    function() {
+      cb(server);
+    }
+  );
+}
+
+function web_server() {
+  const express = require('express');
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.use('/', express.static("."));
+
+  let server = app.listen(0,
+    function () {
+      let sport = server.address().port;
+      ws_server(function(ws) {
+        let wport = ws.address().port;
+        console.log(mutil.make_message('DISPLAY', 'create_window',{'url':`http://localhost:${sport}/index.html?data=${wport}`}));
+      });
+    }
+  );
+}
+
+web_server();
--- System/Kernel/main.js
+++ System/Kernel/main.js
@@ -1,4 +1,43 @@
-function procserver() {
+const mutil = require('shared/messages');
+const registry = require('shared/registry');
+
+function route_message(msg, ctx) {
+  const sio = require('shared/client_ws');
+  console.error(msg);
+
+  if (msg.command == 'register' && msg.options.name && ctx) {
+    registry.set_object(msg.options.name, ctx);
+  }
+  else if (msg.command == 'unregister') {
+  }
+  else {
+    let con = registry.get_object(msg.target);
+    if (con) {
+      sio.send_message(con, msg.target, msg.command, msg.options);
+    }
+  }
+}
+
+function ws_receive_message(connection) {
+  connection.on('message', 
+    function(message) {
+      let str = message.utf8Data;
+      let msg = mutil.parse_message(str);
+      if (msg) {
+        route_message(msg, connection);
+      }
+    }
+  );
+
+  connection.on('close', 
+    function(reasonCode, description) {
+      registry.remove_all_objects(connection);
+      console.error('disconnected: ' + connection.remoteAddress);
+    }
+  );
+}
+
+function ws_server() {
   const WebSocketServer = require('websocket').server;
   const http = require('http');
   const port = (process.env.KERNEL_WSPORT || 4000);
@@ -6,30 +45,24 @@ function procserver() {
   let server = http.createServer();
 
   let wsServer = new WebSocketServer({
-    httpServer: server
+    httpServer:server
   });
 
-  wsServer.on('request', function(request) {
-    console.log('req');
-    let connection = request.accept(null, request.origin);
-    connection.send('data');
-
-    connection.on('message', function(message) {
-      let str = message.utf8Data;
-    });
-
-    connection.on('close', function(reasonCode, description) {
-      console.error((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-  });
+  wsServer.on('request', 
+    function(request) {
+      console.error('new request');
+      let connection = request.accept(null, request.origin);
+      ws_receive_message(connection);
+    }
+  );
 
   server.listen(port, function() {
-    console.error('kernel ws port ' + port);
+    console.error(`kernel ws://localhost:${port}`);
   });
 }
 
-function webserver() {
-  const reg = require('./registry.js');
+function web_server() {
+  const reg = require('shared/registry');
   const express = require('express');
   const fshandler = require('./filesystem.js');
   const app = express();
@@ -41,10 +74,26 @@ function webserver() {
 
   app.get('*', fshandler.handle_file_request);
 
-  app.listen(port, function() {
-    console.error(`kernel url http://localhost:${port}`);
-  });
+  app.listen(port, 
+    function() {
+      console.error(`kernel http://localhost:${port}`);
+    }
+  );
 }
 
-webserver();
-procserver();
+function io_client() {
+  const cio = require('shared/client_io');
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+
+  cio.receive_message(process.stdin, 
+    function(msg) {
+      route_message(msg, null);
+    }
+  );
+}
+
+web_server();
+ws_server();
+io_client();
